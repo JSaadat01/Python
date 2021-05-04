c004c89bb099bd7098ed87a5194f049e795065ab
Jalal Saadat <jalal123saadat@gmail.com>
2021-05-04 13:42:47 +0100

    updated Dice.py

--- Dice.py
+++ Dice.py
@@ -1,8 +1,10 @@
 import random
 
+#Create a Dice 1-6
 def dice():
     di = random.randint(1,6)
     return di
 
 di = dice()
-print(di)
+di2 = dice()
+print(f'You have just rolled two dices and the result is: {di} & {di2}.')
